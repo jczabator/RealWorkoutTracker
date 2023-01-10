2d88cbaf23bde6e5df516b34342c5329cdc2a414
Jakub Czabator <jczabator@gmail.com>
2023-01-10 03:37:24 +0100

    format workoutData with prettier

--- data/workout.tsx
+++ data/workout.tsx
@@ -1,137 +1,145 @@
 export const workoutData = [
-{
-    "name": "Barbell row",            
-    "equipmentType": "small barbell",
-    "setCollection": [
-        {
-            "weight": 50,
-            "numberOfSets": 4,
-            "numberOfRepetitions": 10
-        }]
-},
-{            
-    "name": "Barbell chest press inclined",            
-    "equipmentType": "big barbell",
-    "setCollection": [
-        {
-            "weight": 25,
-            "numberOfSets": 2,
-            "numberOfRepetitions": 6
-        },
-        {
-            "weight": 25,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 5
-        },
-        {
-            "weight": 22.5,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 5
-        },
-        {
-            "weight": 20,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 6
-        },
-        {
-            "weight": 15,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 6
-        },
-        {
-            "weight": 10,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 6
-        }]
-},
-{
-    "name": "Barbell squats",            
-    "equipmentType": "big barbell",
-    "setCollection": [
-        {
-            "weight": 30,
-            "numberOfSets": 4,
-            "numberOfRepetitions": 10
-        },
-        {
-            "weight": 25,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 10
-        },
-        {
-            "weight": 20,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 10
-        },
-        {
-            "weight": 15,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 10
-        },
-        {
-            "weight": 10,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 10
-        }]
-},
-{
-    "name": "Deadlift",            
-    "equipmentType": "small barbell",
-    "setCollection": [
-        {
-            "weight": 80,
-            "numberOfSets": 3,
-            "numberOfRepetitions": 10
-        }]
-},
-{
-    "name": "Shoulder press",            
-    "equipmentType": "dumbbell",
-    "setCollection": [
-        {
-            "weight": 20,
-            "numberOfSets": 4,
-            "numberOfRepetitions": 8
-        }]
-},
-{
-    "name": "Shoulder press standing",            
-    "equipmentType": "small barbell",
-    "setCollection": [
-        {
-            "weight": 40,
-            "numberOfSets": 4,
-            "numberOfRepetitions": 10
-        }]
-},
-{
-    "name": "Barbell chest press normal",            
-    "equipmentType": "big barbell",
-    "setCollection": [
-        {
-            "weight": 20,
-            "numberOfSets": 3,
-            "numberOfRepetitions": 6
-        }]
-}
-,
-{
-    "name": "Pull-ups on machine",            
-    "equipmentType": "small barbell",
-    "setCollection": [
-        {
-            "weight": 25,
-            "numberOfSets": 3,
-            "numberOfRepetitions": 8
-        },
-        {
-            "weight": 30,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 8
-        },
-        {
-            "weight": 35,
-            "numberOfSets": 1,
-            "numberOfRepetitions": 10
-        }]
-}]
+  {
+    name: "Barbell row",
+    equipmentType: "small barbell",
+    setCollection: [
+      {
+        weight: 50,
+        numberOfSets: 4,
+        numberOfRepetitions: 10,
+      },
+    ],
+  },
+  {
+    name: "Barbell chest press inclined",
+    equipmentType: "big barbell",
+    setCollection: [
+      {
+        weight: 25,
+        numberOfSets: 2,
+        numberOfRepetitions: 6,
+      },
+      {
+        weight: 25,
+        numberOfSets: 1,
+        numberOfRepetitions: 5,
+      },
+      {
+        weight: 22.5,
+        numberOfSets: 1,
+        numberOfRepetitions: 5,
+      },
+      {
+        weight: 20,
+        numberOfSets: 1,
+        numberOfRepetitions: 6,
+      },
+      {
+        weight: 15,
+        numberOfSets: 1,
+        numberOfRepetitions: 6,
+      },
+      {
+        weight: 10,
+        numberOfSets: 1,
+        numberOfRepetitions: 6,
+      },
+    ],
+  },
+  {
+    name: "Barbell squats",
+    equipmentType: "big barbell",
+    setCollection: [
+      {
+        weight: 30,
+        numberOfSets: 4,
+        numberOfRepetitions: 10,
+      },
+      {
+        weight: 25,
+        numberOfSets: 1,
+        numberOfRepetitions: 10,
+      },
+      {
+        weight: 20,
+        numberOfSets: 1,
+        numberOfRepetitions: 10,
+      },
+      {
+        weight: 15,
+        numberOfSets: 1,
+        numberOfRepetitions: 10,
+      },
+      {
+        weight: 10,
+        numberOfSets: 1,
+        numberOfRepetitions: 10,
+      },
+    ],
+  },
+  {
+    name: "Deadlift",
+    equipmentType: "small barbell",
+    setCollection: [
+      {
+        weight: 80,
+        numberOfSets: 3,
+        numberOfRepetitions: 10,
+      },
+    ],
+  },
+  {
+    name: "Shoulder press",
+    equipmentType: "dumbbell",
+    setCollection: [
+      {
+        weight: 20,
+        numberOfSets: 4,
+        numberOfRepetitions: 8,
+      },
+    ],
+  },
+  {
+    name: "Shoulder press standing",
+    equipmentType: "small barbell",
+    setCollection: [
+      {
+        weight: 40,
+        numberOfSets: 4,
+        numberOfRepetitions: 10,
+      },
+    ],
+  },
+  {
+    name: "Barbell chest press normal",
+    equipmentType: "big barbell",
+    setCollection: [
+      {
+        weight: 20,
+        numberOfSets: 3,
+        numberOfRepetitions: 6,
+      },
+    ],
+  },
+  {
+    name: "Pull-ups on machine",
+    equipmentType: "small barbell",
+    setCollection: [
+      {
+        weight: 25,
+        numberOfSets: 3,
+        numberOfRepetitions: 8,
+      },
+      {
+        weight: 30,
+        numberOfSets: 1,
+        numberOfRepetitions: 8,
+      },
+      {
+        weight: 35,
+        numberOfSets: 1,
+        numberOfRepetitions: 10,
+      },
+    ],
+  },
+];
