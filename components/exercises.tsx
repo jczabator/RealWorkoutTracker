2b7628ff9f016d6a36d45a4487ed6b7380545492
Jakub Czabator <jczabator@gmail.com>
2023-01-12 00:49:14 +0100

    Use grid and flex to show multiple cards in one row

--- components/exercises.tsx
+++ components/exercises.tsx
@@ -2,25 +2,33 @@ import React from "react";
 import styles from '../styles/Home.module.css'
 import { workoutData } from "../data/workout";
 import { Exercise } from "./exercise";
+import Grid from "@material-ui/core/Grid";
 
 export const Exercises = () => {
     return (
         <>
         <div className="exercise-container">
+            <Grid
+                container
+                spacing={2}
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start">
             {
                 workoutData.map((data, key) => {
                     return (
-                        <div key={key}>
+                        <Grid item xs={12} sm={6} md={3} key={key}>
                             <Exercise
                                 key={key}
                                 name={data.name}
                                 equipmentType={data.equipmentType}
                                 setCollection={data.setCollection}
-                            />                                                        
-                        </div>                        
+                            />                                                          
+                        </Grid>
                     );
                 })
             }
+            </Grid>
         </div>
         </>
     )
